4c657e20d2395b80b37dfb48969de51134f9036e
zeinmzdh <zeinmzadeh@gmail.com>
2022-01-21 19:26:12 +0330

    shortest job first sched finished

--- main.py
+++ main.py
@@ -2,8 +2,32 @@ from task import Task
 
 
 def first_come_first_served(tasks_list):
-    time = 0;
+    time = 0
     while len(tasks_list) != 0 :
+        running_task = tasks_list.pop(0)
+        while running_task.has_time_left():
+            print('t:', time, 'running task:', running_task, 'seconds_passed:', running_task.time_completed,
+                  'time_left:', running_task.time_left)
+            print('ready list:', tasks_list)
+            if running_task.time_left >= 1:
+                time = time + 1
+                running_task.time_left = running_task.time_left - 1
+                running_task.time_completed = running_task.time_completed + 1
+            else:
+                time = time + running_task.time_left
+                running_task.time_left = running_task.time_left - running_task.time_left
+                running_task.time_completed = running_task.time_completed + running_task.time_left
+        print('-------------------------------------------------------------')
+
+        print('t:', time, 'task_completed:', running_task, 'seconds_passed:', running_task.time_completed)
+        print('-------------------------------------------------------------')
+
+
+def shortest_job_first(tasks_list):
+    tasks_list.sort(key=lambda x: x.duration)
+    print(tasks_list)
+    time = 0
+    while len(tasks_list) != 0:
         running_task = tasks_list.pop(0)
         while running_task.has_time_left():
             print('t:', time, 'running task:', running_task, 'seconds_passed:', running_task.time_completed,
@@ -24,10 +48,6 @@ def first_come_first_served(tasks_list):
     pass
 
 
-def shortest_job_first(tasks_list):
-    pass
-
-
 def round_robin(tasks_list):
     pass
 
@@ -44,5 +64,5 @@ if __name__ == '__main__':
         task = Task(n, t, d)
         tasks.append(task)
 
-    first_come_first_served(tasks)
+    shortest_job_first(tasks)
 
