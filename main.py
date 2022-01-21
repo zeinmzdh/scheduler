ef3e3025acf3b73d63d9fe6076d94be6fdb72a41
zeinmzdh <zeinmzadeh@gmail.com>
2022-01-21 19:21:09 +0330

    first come first served sched finished

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from task import Task
 
-if __name__ == '__main__':
-    tasks_count = int(input())
-    tasks = []
-    for i in range(tasks_count):
-        n, t, d = input().split()
-        task = Task(n, t, d)
-        tasks.append(task)
-
 
 def first_come_first_served(tasks_list):
+    time = 0;
+    while len(tasks_list) != 0 :
+        running_task = tasks_list.pop(0)
+        while running_task.has_time_left():
+            print('t:', time, 'running task:', running_task, 'seconds_passed:', running_task.time_completed,
+                  'time_left:', running_task.time_left)
+            print('ready list:', tasks_list)
+            if running_task.time_left >= 1:
+                time = time + 1
+                running_task.time_left = running_task.time_left - 1
+                running_task.time_completed = running_task.time_completed + 1
+            else:
+                time = time + running_task.time_left
+                running_task.time_left = running_task.time_left - running_task.time_left
+                running_task.time_completed = running_task.time_completed + running_task.time_left
+        print('-------------------------------------------------------------')
+
+        print('t:', time, 'task_completed:', running_task, 'seconds_passed:', running_task.time_completed)
+        print('-------------------------------------------------------------')
     pass
 
 
@@ -23,4 +34,15 @@ def round_robin(tasks_list):
 
 def highest_response_ratio_first(tasks_list):
     pass
+
+
+if __name__ == '__main__':
+    tasks_count = int(input())
+    tasks = []
+    for i in range(tasks_count):
+        n, t, d = input().split()
+        task = Task(n, t, d)
+        tasks.append(task)
+
+    first_come_first_served(tasks)
 
--- task.py
+++ task.py
@@ -3,9 +3,14 @@ class Task:
     def __init__(self, name, task_type, duration):
         self.name = name
         self.task_type = task_type
-        self.duration = duration
-        self.time_left = duration
+        self.duration = float(duration)
+        self.time_left = float(duration)
         self.time_completed = 0
+
+    def has_time_left(self):
+        if self.time_left > 0:
+            return True
+        return False
 
     def __str__(self):
         return self.name
