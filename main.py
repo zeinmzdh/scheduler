e341c4e3f48282cf11996f5e6eb9fd3f198a0502
zeinmzdh <zeinmzadeh@gmail.com>
2022-01-24 11:12:05 +0330

    highest response ratio next added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from task import Task
 
 def first_come_first_served(tasks_list):
     time = 0
-    while len(tasks_list) != 0 :
+    while len(tasks_list) != 0:
         running_task = tasks_list.pop(0)
         while running_task.has_time_left():
             print('t:', time, 'running task:', running_task, 'seconds_passed:', running_task.time_completed,
@@ -70,8 +70,49 @@ def round_robin(tasks_list):
             tasks_list.append(running_task)
 
 
-def highest_response_ratio_first(tasks_list):
-    pass
+def highest_response_ratio_next(tasks_list):
+    time = 0
+    ready_list = []
+    ready_list = ready_list + check_for_new_tasks(tasks_list, time)
+    while len(ready_list) != 0:
+        running_task = ready_list.pop(0)
+        while running_task.has_time_left():
+            ready_list = ready_list + check_for_new_tasks(tasks_list, time)
+            for tsk in ready_list:
+                tsk.update_waiting_time_and_rr_time(time)
+            ready_list.sort(key=lambda x: x.response_ratio)
+            print('t:', time, 'running task:', running_task, 'seconds_passed:', running_task.time_completed,
+                  'time_left:', running_task.time_left)
+            print('ready list:', ready_list)
+            if running_task.time_left >= 1:
+                time = time + 1
+                running_task.time_left = running_task.time_left - 1
+                running_task.time_completed = running_task.time_completed + 1
+            else:
+                time = time + running_task.time_left
+                running_task.time_left = running_task.time_left - running_task.time_left
+                running_task.time_completed = running_task.time_completed + running_task.time_left
+
+        ready_list = ready_list + check_for_new_tasks(tasks_list, time)
+        for tsk in ready_list:
+            tsk.update_waiting_time_and_rr_time(time)
+        ready_list.sort(key=lambda x: x.response_ratio)
+        # for x in ready_list:
+        #     print(x.name, ": ", x.response_ratio)
+        # print(ready_list)
+        print('-------------------------------------------------------------')
+        print('t:', time, 'task_completed:', running_task, 'seconds_passed:', running_task.time_completed)
+        print('ready list:', ready_list)
+        print('-------------------------------------------------------------')
+
+
+def check_for_new_tasks(tasks_list, curr_time):
+    tmp = []
+    for i, tsk in enumerate(tasks_list):
+        if tsk.arrival_time <= curr_time:
+            tmp.append(tsk)
+            tasks_list.pop(i)
+    return tmp
 
 
 if __name__ == '__main__':
@@ -79,8 +120,7 @@ if __name__ == '__main__':
     tasks = []
     for i in range(tasks_count):
         n, t, d = input().split()
-        task = Task(n, t, d)
+        task = Task(n, t, d, i)
         tasks.append(task)
 
-    round_robin(tasks)
-
+    highest_response_ratio_next(tasks)
--- task.py
+++ task.py
@@ -1,16 +1,25 @@
 
 class Task:
-    def __init__(self, name, task_type, duration):
+    def __init__(self, name, task_type, duration, arrival_time):
         self.name = name
         self.task_type = task_type
         self.duration = float(duration)
         self.time_left = float(duration)
         self.time_completed = 0
+        self. arrival_time = arrival_time
+        self.waiting_time = 0
+        self.response_ratio = (self.duration + self.waiting_time) / self.duration
 
     def has_time_left(self):
         if self.time_left > 0:
             return True
         return False
+
+    def update_waiting_time_and_rr_time(self, curr_time):
+        self.waiting_time = curr_time - self.arrival_time
+        # print(self.waiting_time)
+        self.response_ratio = (self.duration + self.waiting_time) / self.duration
+        # print(self.response_ratio,  (self.duration + self.waiting_time) / self.duration)
 
     def __str__(self):
         return self.name
